fa9a731214987edf63d9ea331195ef4198e6b332
Caqil <cakrabudiman@icloud.com>
2023-12-03 05:25:09 +0000

    modified:   src/lib/Server.js

--- src/lib/Server.js
+++ src/lib/Server.js
@@ -98,9 +98,8 @@ module.exports = class Server {
       }))
       .get('/api/wireguard/client/:clientId/configuration', Util.promisify(async (req, res) => {
         const { clientId } = req.params;
-        const client = await WireGuard.getClient({ clientId });
         const config = await WireGuard.getClientConfiguration({ clientId });
-        const configName = client;
+        const configName = clientId;
         res.header('Content-Disposition', `attachment; filename="${configName}.conf"`);
         res.header('Content-Type', 'text/plain');
         res.send(config);
